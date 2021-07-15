33dd7c0df82cec1526e03f07e30c1734d0ab52a1
Jan T. Sott <git@idleberg.com>
2021-07-15 23:40:32 +0200

    first commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,27 @@
+/* eslint-disable */
 module.exports = {
-  // JS Files
-  "root": true,
-  "env": {
-    "atomtest": true,
-    "es6": true,
-    "node": true,
-    "browser": true,
-    "jasmine": true
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
   },
-  "globals": { "atom": "readonly"},
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": { "jsx": true },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:json/recommended'
   ],
-  "rules": {
-    "semi": [2, "always"],
-    "no-prototype-builtins": "off"
+  globals: {
+    atom: "readonly"
   },
-  "ignorePatterns": [
-    "lib/",
-    "node_modules/"
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
   ],
-  "overrides": [
-    {
-      // JSON files
-      "files": ["*.json"],
-      "plugins": ["json"],
-      "extends": ["plugin:json/recommended"],
-      "rules": {
-        "json/*": ["error", {"allowComments": true}]
-      }
-    }
+  rules: {},
+  ignorePatterns: [
+    'bower_components',
+    'lib',
+    'node_modules'
   ]
 };
